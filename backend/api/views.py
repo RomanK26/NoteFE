5e197dee97e3375fc86cc1aa6fb3312aced80d98
RomanK26 <rkasichhwa1@gmail.com>
2025-07-11 22:26:49 +0545

    deployment testing

---
+++ backend/api/views.py
@@ -0,0 +1,37 @@
+from logging import raiseExceptions
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.generics import CreateAPIView,ListCreateAPIView,RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import AllowAny,IsAuthenticated
+from .models import Note
+from api.serializers import NoteSerializer, UserSerializer
+# Create your views here.
+
+
+class RegistrationView(CreateAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    
+    
+class NoteListCreateView(ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated,]
+    
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author = user)
+    
+    def perform_create(self, serializer):
+        serializer.save(author = self.request.user)
+        
+class NoteUpdateDetailDeleteView(RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated,]
+    serializer_class = NoteSerializer
+    queryset = Note.objects.all()
+            
+    def get_object(self):
+        obj = super().get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied("You do not have permission to access this note.")
+        return obj
