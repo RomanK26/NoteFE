2d769243544f17ddc29f66c7c97c016355dda75e
RomanK26 <rkasichhwa1@gmail.com>
2025-07-16 16:24:31 +0545

    feat: add caching and update permissions

--- api/views.py
+++ api/views.py
@@ -1,11 +1,12 @@
-from logging import raiseExceptions
-
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.core.cache import cache
+from rest_framework import filters
 from rest_framework.generics import (CreateAPIView, ListCreateAPIView,
                                      RetrieveUpdateDestroyAPIView)
-from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework import filters
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+from api.permissions import IsOwner
 from api.serializers import NoteSerializer, UserSerializer
 
 from .models import Note
@@ -17,28 +18,37 @@ class RegistrationView(CreateAPIView):
     permission_classes = [AllowAny]
     serializer_class = UserSerializer
     queryset = User.objects.all()
-    
-    
+
+
 class NoteListCreateView(ListCreateAPIView):
     serializer_class = NoteSerializer
-    permission_classes = [IsAuthenticated,]
+    permission_classes = [IsOwner,]
     filter_backends = [filters.SearchFilter]
     search_fields = ['title', 'content']
-    
+
     def get_queryset(self):
         user = self.request.user
-        return Note.objects.filter(author = user)
-    
+        notes = Note.objects.filter(author=user)
+        return notes
+
+    def list(self, request, *args, **kwargs):
+        user = self.request.user
+        cache_key = f"notes_user_{user.id}"
+        cached_data = cache.get(cache_key)
+
+        if cached_data:
+            return Response(cached_data)
+
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        cache.set(cache_key, serializer.data, timeout=300)
+        return Response(serializer.data)
+
     def perform_create(self, serializer):
-        serializer.save(author = self.request.user)
-        
+        serializer.save(author=self.request.user)
+
+
 class NoteUpdateDetailDeleteView(RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated,]
+    permission_classes = [IsOwner,]
     serializer_class = NoteSerializer
     queryset = Note.objects.all()
-            
-    def get_object(self):
-        obj = super().get_object()
-        if obj.author != self.request.user:
-            raise PermissionDenied("You do not have permission to access this note.")
-        return obj
